16f6fb2c6a1a05cf3bd46a1b4613b7fe5dbc446e
naveen magnur <naveen magnur@magnur>
2022-07-18 16:00:22 +0530

    added by sanjay

--- src/test/java/SampleOrganizationTest.java
+++ src/test/java/SampleOrganizationTest.java
@@ -9,10 +9,14 @@ public class SampleOrganizationTest {
 	@Test(groups = "smoke test")
 	public void createOrganizationTest() {
 	System.out.println("=========organization created==========");
+	
+	System.out.println("this statment is added by sanjay");
 	}
 
 	@Test(groups="regression test")
 	public void editOrganizationTest() {
 	System.out.println("========edit organization========");
+	
+	System.out.println("this statment is added by sanjay ");
 	}
 }
